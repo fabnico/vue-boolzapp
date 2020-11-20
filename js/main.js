71000ae61780d87c73711aaaa8c4e3c9ff244822
Fabrizio <whoet.wo@gmail.com>
2020-11-20 17:27:58 +0100

    Introduzione JS lista contatti

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const myApp = new Vue({
+   el: '#root',
+   data:{
+      contactsArray: [
+         {
+            avatar: 'img/avatar_1.jpg',
+            name: 'Michele',
+            lastAccess: ''
+         },
+         {
+            avatar: 'img/avatar_2.jpg',
+            name: 'Fabio',
+            lastAccess: ''
+         },
+         {
+            avatar: 'img/avatar_3.jpg',
+            name: 'Samuele',
+            lastAccess: ''
+         },
+         {
+            avatar: 'img/avatar_4.jpg',
+            name: 'Luisa',
+            lastAccess: ''
+         }
+      ]
+   },
+   methods:{
+
+   }
+})
