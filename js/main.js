1b74572b8bc593bd50f7cacdc8ff87f7559c33b7
Fabrizio <whoet.wo@gmail.com>
2020-11-20 17:37:47 +0100

    Aggiunta JS nomeUtente

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const myApp = new Vue({
    el: '#root',
    data:{
+      mainUser: [
+         {
+            avatar: 'img/avatar_io.jpg',
+            name: 'Fabrizio'
+         }
+      ],
       contactsArray: [
          {
             avatar: 'img/avatar_1.jpg',
